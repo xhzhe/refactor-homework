e0c8b4e3f524227e11af16acae22e4606d7ddc51
xhz <747367352@qq.com>
2020-08-27 20:56:58 +0800

    [alex] refactor: extract magic variable to const

--- src/delivery.js
+++ src/delivery.js
@@ -1,15 +1,30 @@
+const MA_CT = [
+    'MA',
+    'CT',
+];
+
+const NY_NH = [
+    'NY',
+    'NH',
+];
+
+const MA_CT_NY = [
+    'MA',
+    'CT',
+    'NY',
+];
+
+const ME_NH = [
+    'ME',
+    'NH',
+];
+
 function deliveryDate(anOrder, isRush) {
     if (isRush) {
         let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-        ].includes(anOrder.deliveryState)) {
+        if (MA_CT.includes(anOrder.deliveryState)) {
             deliveryTime = 1;
-        } else if ([
-            'NY',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
+        } else if (NY_NH.includes(anOrder.deliveryState)) {
             deliveryTime = 2;
         } else {
             deliveryTime = 3;
@@ -17,16 +32,9 @@ function deliveryDate(anOrder, isRush) {
         return anOrder.placedOn.plusDays(1 + deliveryTime);
     } else {
         let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-            'NY',
-        ].includes(anOrder.deliveryState)) {
+        if (MA_CT_NY.includes(anOrder.deliveryState)) {
             deliveryTime = 2;
-        } else if ([
-            'ME',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
+        } else if (ME_NH.includes(anOrder.deliveryState)) {
             deliveryTime = 3;
         } else {
             deliveryTime = 4;
