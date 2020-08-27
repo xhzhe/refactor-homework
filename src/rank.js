db87f38935177aea14d53f9fc1fc0d44402bf8ac
xhz <747367352@qq.com>
2020-08-27 21:35:50 +0800

    [alex] refactor: extract code to function

--- src/rank.js
+++ src/rank.js
@@ -27,20 +27,30 @@ function captainHistoryRisk(voyage, history) {
     return Math.max(result, 0);
 }
 
-function voyageProfitFactor(voyage, history) {
+function generateFactorOfChina(history, voyage) {
+    let result = 3;
+    result += history.length > 10 ? 1 : 0;
+    result += voyage.length > 12 > 1 ? 1 : 0;
+    result += voyage.length > 18 ? -1 : 0;
+    return result;
+}
+
+function generateFactorNotChina(history, voyage) {
+    let result = history.length > 8 ? 1 : 0;
+    result += voyage.length > 14 ? -1 : 0;
+    return result;
+}
+
+function defaultGenerateFactor(voyage) {
     let result = 2;
     result += voyage.zone === china ? 1 : 0;
     result += voyage.zone === eastIndies ? 1 : 0;
-    if (voyage.zone === china && hasChina(history)) {
-        result += 3;
-        result += history.length > 10 ? 1 : 0;
-        result += voyage.length > 12 > 1 ? 1 : 0;
-        result += voyage.length > 18 ? -1 : 0;
-    } else {
-        result += history.length > 8 ? 1 : 0;
-        result += voyage.length > 14 ? -1 : 0;
-    }
     return result;
+}
+
+function voyageProfitFactor(voyage, history) {
+    return defaultGenerateFactor(voyage) + (voyage.zone === china && hasChina(history) ?
+        generateFactorOfChina(history, voyage) : generateFactorNotChina(history, voyage));
 }
 
 function rating(voyage, history) {
