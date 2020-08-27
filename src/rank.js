b241e5185b0feef97b792354ed50310c1d87ea88
xhz <747367352@qq.com>
2020-08-27 20:02:30 +0800

    [alex] feature: add test for captainHistoryRisk

--- src/rank.js
+++ src/rank.js
@@ -74,27 +74,7 @@ function rating(voyage, history) {
 
 module.exports = {
     voyageRisk,
+    captainHistoryRisk,
+    rating,
+    voyageProfitFactor
 };
-
-const voyage = {
-    zone: 'west-indies',
-    length: 10,
-};
-const history = [
-    {
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    },
-    {
-        zone: 'west-africa',
-        profit: 7,
-    },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
--- test/rankTest.js
+++ test/rankTest.js
@@ -1,5 +1,27 @@
 const rankTest = require('ava');
+const {captainHistoryRisk} = require("../src/rank");
 const {voyageRisk} = require("../src/rank");
+
+const voyage = {
+    zone: 'west-indies',
+    length: 10,
+};
+const history = [
+    {
+        zone: 'east-indies',
+        profit: 5,
+    }, {
+        zone: 'west-indies',
+        profit: 15,
+    }, {
+        zone: 'china',
+        profit: -2,
+    },
+    {
+        zone: 'west-africa',
+        profit: 7,
+    },
+];
 
 rankTest('test voyageRisk', t => {
     t.is(voyageRisk({
@@ -20,4 +42,14 @@ rankTest('test voyageRisk', t => {
         length: 10,
         zone: 'china'
     }), 9)
+})
+
+rankTest('test captainHistoryRisk', t => {
+    t.is(captainHistoryRisk({
+        length: 10,
+        zone: 'china'
+    }, history), 4)
+})
+rankTest('test captainHistoryRisk', t => {
+    t.is(captainHistoryRisk(voyage, history), 6)
 })
