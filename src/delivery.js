5c578a29e50123d8c433e78127cc1629c0fb70d7
xhz <747367352@qq.com>
2020-08-27 20:58:51 +0800

    [alex] refactor: extract code to function

--- src/delivery.js
+++ src/delivery.js
@@ -19,27 +19,35 @@ const ME_NH = [
     'NH',
 ];
 
+function rushDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (MA_CT.includes(anOrder.deliveryState)) {
+        deliveryTime = 1;
+    } else if (NY_NH.includes(anOrder.deliveryState)) {
+        deliveryTime = 2;
+    } else {
+        deliveryTime = 3;
+    }
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function defaultDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (MA_CT_NY.includes(anOrder.deliveryState)) {
+        deliveryTime = 2;
+    } else if (ME_NH.includes(anOrder.deliveryState)) {
+        deliveryTime = 3;
+    } else {
+        deliveryTime = 4;
+    }
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
 function deliveryDate(anOrder, isRush) {
     if (isRush) {
-        let deliveryTime;
-        if (MA_CT.includes(anOrder.deliveryState)) {
-            deliveryTime = 1;
-        } else if (NY_NH.includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 3;
-        }
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
+        return rushDeliveryDate(anOrder);
     } else {
-        let deliveryTime;
-        if (MA_CT_NY.includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else if (ME_NH.includes(anOrder.deliveryState)) {
-            deliveryTime = 3;
-        } else {
-            deliveryTime = 4;
-        }
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
+        return defaultDeliveryDate(anOrder);
     }
 }
 module.exports = {deliveryDate}
