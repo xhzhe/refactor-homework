4453671d090b213df9200fca8b9bdc3cdf09fa4c
alex <alex.h.z.xu@oocl.com>
2020-08-28 12:24:10 +0800

    [alex] refactor: simplify sum code to reduce

--- src/print.js
+++ src/print.js
@@ -7,11 +7,7 @@ function generateOwes(invoice, outstanding) {
 }
 
 function printOwing(invoice) {
-    let outstanding = 0;
-    // calculate outstanding
-    for (const o of invoice.borderSpacing) {
-        outstanding += o.amount;
-    }
+    let outstanding = invoice.borderSpacing.reduce((pre, cur) => pre + cur.amount, 0)
     // record due date
     const today = new Date();
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
