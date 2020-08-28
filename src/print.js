32c51cf5d6e09e4c2d85d9ec613b7f6393ccbfcd
alex <alex.h.z.xu@oocl.com>
2020-08-28 11:41:32 +0800

    [alex] refactor: extract result code to function

--- src/print.js
+++ src/print.js
@@ -1,18 +1,20 @@
-function printOwing(invoice) {
-    let outstanding = 0;
+function generateOwes(invoice, outstanding) {
     let result = "***********************\n**** Customer Owes ****\n***********************\n";
-    // calculate outstanding
-    for (const o of invoice.borderSpacing) {
-        outstanding += o.amount;
-    }
-
-    // record due date
-    const today = new Date();
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
     result += `name: ${invoice.customer}\n`;
     result += `amount: ${outstanding}\n`;
     result += `date: ${invoice.dueDate.toLocaleDateString()}\n`;
     return result;
+}
+
+function printOwing(invoice) {
+    let outstanding = 0;
+    // calculate outstanding
+    for (const o of invoice.borderSpacing) {
+        outstanding += o.amount;
+    }
+    // record due date
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    return generateOwes(invoice, outstanding);
 }
 module.exports = printOwing
